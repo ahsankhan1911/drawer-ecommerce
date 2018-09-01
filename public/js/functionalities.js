37b5c31090399b3d01f10d19c287543996970374
ahsankhan1911 <ahsankhan1911@gmail.com>
2018-09-01 21:35:37 +0500

    carousel added, featured products added

--- public/js/functionalities.js
+++ public/js/functionalities.js
@@ -1,23 +1,15 @@
-// function handleActive (event) {
-// console.log(event)
-// }
-// console.log("hellooo 22")
-
-
-// // handleActive()
-console.log(window.location.pathname)
-
-
 
 switch (window.location.pathname) {
 
     case '/contact':
+
         document.getElementById('contact').className = 'active';
         break;
 
     case '/products':
 
-        document.getElementById('products').className = 'active';
+
+        document.getElementById('product').className = 'active';
         break;
 
     default:
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 app.use(express.static('public'));
 
+//body parser middleware
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
 
 
 app.get('/', function(req, res) {
@@ -12,6 +17,12 @@ app.get('/', function(req, res) {
 
 app.get('/contact', function(req, res) {
     res.sendFile(__dirname + '/public/pages/contact.html');
+  });
+
+app.post('/contact', function(req, res) {
+   res.send(req.body)
+
+
   });
   
 app.get('/products', function(req, res) {
